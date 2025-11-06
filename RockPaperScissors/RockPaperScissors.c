8fe6db9a0479e137e1415fcab8edc65d776cc430
OluwaloniAyeni <ayenimidun@gmail.com>
2025-11-06 18:41:43 -0500

    Fix Paper Loses To Scissors condition - test passed (Green Phase)

--- RockPaperScissors/RockPaperScissors.c
+++ RockPaperScissors/RockPaperScissors.c
@@ -15,5 +15,8 @@ const char* determineWinner(const char player1[], const char player2[])
 	if (strcmp(player2, "Rock") == 0 && strcmp(player1, "Scissors") == 0)
 		return "Player2";
 
+	if (strcmp(player2, "Scissors") == 0 && strcmp(player1, "Paper") == 0)
+		return "Player2";
+
 	    return "Invalid";
 }
