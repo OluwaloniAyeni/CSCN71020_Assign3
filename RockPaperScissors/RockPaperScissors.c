d3811bcd6b13eb2070f888924f5ff42c2205b622
OluwaloniAyeni <ayenimidun@gmail.com>
2025-11-06 17:27:06 -0500

    Refactor determineWinner function after first passing test (TDD Blue Phase

--- RockPaperScissors/RockPaperScissors.c
+++ RockPaperScissors/RockPaperScissors.c
@@ -5,6 +5,6 @@ const char* determineWinner(const char player1[], const char player2[])
 {
 	if (strcmp(player1, "Rock") == 0 && strcmp(player2, "Scissors") == 0)
 		return "Player1";
-	else
+
 	    return "Invalid";
 }
