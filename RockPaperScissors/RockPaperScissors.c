ce3162ccc29aac9c833681d64b640c1b09094f1e
OluwaloniAyeni <ayenimidun@gmail.com>
2025-11-06 17:55:13 -0500

    Fix Scissors Beats Paper condition - test passed (TDD Green Phase)

--- RockPaperScissors/RockPaperScissors.c
+++ RockPaperScissors/RockPaperScissors.c
@@ -6,5 +6,8 @@ const char* determineWinner(const char player1[], const char player2[])
 	if (strcmp(player1, "Rock") == 0 && strcmp(player2, "Scissors") == 0)
 		return "Player1";
 
+	if (strcmp(player1, "Scissors") == 0 && strcmp(player2, "Paper") == 0)
+		return "Player1";
+
 	    return "Invalid";
 }
