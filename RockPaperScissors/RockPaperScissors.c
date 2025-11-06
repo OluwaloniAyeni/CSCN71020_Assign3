5af1b6fbb9df8750c5fd593399ceb954062ecce8
OluwaloniAyeni <ayenimidun@gmail.com>
2025-11-06 18:58:48 -0500

    Implement Draw condition when both moves are the same (TDD Green Phase)

--- RockPaperScissors/RockPaperScissors.c
+++ RockPaperScissors/RockPaperScissors.c
@@ -3,6 +3,10 @@
 
 const char* determineWinner(const char player1[], const char player2[])
 {
+	//Draw check first
+	if (strcmp(player1, player2) == 0)
+		return "Draw";
+
 	if (strcmp(player1, "Rock") == 0 && strcmp(player2, "Scissors") == 0)
 		return "Player1";
 
