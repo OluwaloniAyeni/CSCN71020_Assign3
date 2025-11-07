18860f46deda4ebfaddb04ca59652123533e44df
OluwaloniAyeni <ayenimidun@gmail.com>
2025-11-06 19:18:54 -0500

    Add comments for Player1 and Player2 winning conditions; noted that invalid input test already passed wuthout code change (Blue stage refactor)

--- RockPaperScissors/RockPaperScissors.c
+++ RockPaperScissors/RockPaperScissors.c
@@ -6,7 +6,7 @@ const char* determineWinner(const char player1[], const char player2[])
 	//Draw check first
 	if (strcmp(player1, player2) == 0)
 		return "Draw";
-
+	//Player 1 wins conditions
 	if (strcmp(player1, "Rock") == 0 && strcmp(player2, "Scissors") == 0)
 		return "Player1";
 
@@ -16,6 +16,7 @@ const char* determineWinner(const char player1[], const char player2[])
 	if (strcmp(player1, "Paper") == 0 && strcmp(player2, "Rock") == 0)
 		return "Player1";
 
+	//Player 2 wins conditions
 	if (strcmp(player2, "Rock") == 0 && strcmp(player1, "Scissors") == 0)
 		return "Player2";
 
